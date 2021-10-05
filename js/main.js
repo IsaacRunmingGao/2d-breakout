d1e10760d82bae49d009ce35087183379428c52e
IsaacRunmingGao <runminggao@163.com>
2021-10-04 22:30:23 -0700

    add config

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 import Phaser from 'phaser'
 
+
+export default class GameScene extends Phaser.Scene
+{
+  constructor()
+  {
+    super('game-scene')
+  }
+
+  preload()
+  {
+    this.load.image('ball', '/Users/gaorunming/Desktop/projects/2d-breakout/img/ball.png');
+  }
+
+  create()
+  {
+    this.add.sprite(50, 80, 'ball');
+  }
+}
+
+
 const gameConfig = {
   title: '2D Breakout',
   type: Phaser.AUTO,
@@ -11,21 +31,21 @@ const gameConfig = {
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
   autoFocus: true,
-  // scene:[loadingScene]
+  scene:[GameScene]
 };
+
 
 const game = new Phaser.Game(gameConfig);
 
-//Loader.LoaderPlugin
-const preload = () => {
-  game.load.image('ball', 'img/ball.png');
-}
+//const preload = () => {
+//game.load.image('ball', 'img/ball.png');
+//}
 
-const create = () => {
-  const ball = game.add.sprite(50, 50, 'ball');
-}
+//const create = () => {
+//  const ball = game.add.sprite(50, 50, 'ball');
+//}
 
-const update = () => {
-}
+//const update = () => {
+//}
 
 
